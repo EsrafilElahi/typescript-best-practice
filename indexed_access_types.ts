33f9ba845d0b4b6143cec55e8445f0e7c90b1556
esrafil elahi <esrafil.elahi@gmail.com>
2020-08-22 15:24:25 +0430

    add indexed access file

--- indexed_access_types.ts
+++ indexed_access_types.ts
@@ -1,31 +1,29 @@
 // color
 interface ColorVariants {
-  primary: "blue",
-  secondary: "red",
-  tertiary: "green"
+  primary: "blue";
+  secondary: "red";
+  tertiary: "green";
 }
 
-type PrimaryColor = ColorVariants["primary"] // "blue"
-type NonPrimaryColor = ColorVariants["secondary" | "tertiary"] // "red" | "green"
-type EveryColorVariants_Value = ColorVariants[keyof ColorVariants] // "blue" | "red" | "green"
-type EveryColorVariants_Key = keyof ColorVariants // "primary" "secondary" | "tertiary"
-
+type PrimaryColor = ColorVariants["primary"]; // "blue"
+type NonPrimaryColor = ColorVariants["secondary" | "tertiary"]; // "red" | "green"
+type EveryColorVariants_Value = ColorVariants[keyof ColorVariants]; // "blue" | "red" | "green"
+type EveryColorVariants_Key = keyof ColorVariants; // "primary" "secondary" | "tertiary"
 
 // letters
-type Letters = ["a", "b", "c"]
+type Letters = ["a", "b", "c"];
 
-type AOrB = Letters["0" | "1"] // "a" | "b"
-type AllLettersUnion = Letters[number] // "a" | "b" | "c"
-
+type AOrB = Letters["0" | "1"]; // "a" | "b"
+type AllLettersUnion = Letters[number]; // "a" | "b" | "c"
 
 // userRole
 interface UserRoleConfig {
-  user: ["view", "create", "update"],
-  superAdmin: ["view", "create", "update", "delete"]
+  user: ["view", "create", "update"];
+  superAdmin: ["view", "create", "update", "delete"];
 }
 
-type AllTwoRolesUnion = UserRoleConfig[keyof UserRoleConfig]
+type AllTwoRolesUnion = UserRoleConfig[keyof UserRoleConfig];
 // ["view", "create", "update"] | ["view", "create", "update", "delete"]
 
-type AllRolesUnion = UserRoleConfig[keyof UserRoleConfig][number]
+type AllRolesUnion = UserRoleConfig[keyof UserRoleConfig][number];
 // "view" | "create" | "update" | "delete"
