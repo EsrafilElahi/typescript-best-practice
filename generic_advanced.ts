2ce0c82f0e07c85c606f0851a84c4f18eca6391a
esrafil elahi <esrafil.elahi@gmail.com>
2020-08-26 15:24:23 +0430

    advanced generix types added

--- generic_advanced.ts
+++ generic_advanced.ts
@@ -1,8 +1,12 @@
-type RemoveKeyResultType<Key extends string, Obj> = Omit<Obj, Key>
-export const makeKeyRemover = <Key extends string>(keys: Key[]) => <Obj>(obj: Obj): RemoveKeyResultType<Key, Obj> => { return {} as any }
+type RemoveKeyResultType<Key extends string, Obj> = Omit<Obj, Key>;
+export const makeKeyRemover =
+  <Key extends string>(keys: Key[]) =>
+  <Obj>(obj: Obj): RemoveKeyResultType<Key, Obj> => {
+    return {} as any;
+  };
 
-const keyRemover = makeKeyRemover(["a", "b"])
+const keyRemover = makeKeyRemover(["a", "b"]);
 
-const newObject = keyRemover({ a: 1, b: 2, c: 3 })
+const newObject = keyRemover({ a: 1, b: 2, c: 3 });
 
-newObject.c // just this show
+newObject.c; // just this show
