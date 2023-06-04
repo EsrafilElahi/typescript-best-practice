e5372bde311ab1b910e2376648946e76de0f532e
esrafilelahi <essinho1121@gmail.com>
2023-06-04 17:01:45 +0330

    adding...

---
+++ avoid_using_enum.ts
@@ -0,0 +1,35 @@
+//Bad ❌
+enum BadState1 {
+  InProgress,
+  Success,
+  Fail,
+}
+
+enum BadState2 {
+  InProgress = 10,
+  Success = 20,
+  Fail = 30,
+}
+
+BadState1.InProgress; // (enum member) State.InProgress = 0
+BadState1.Success; // (enum member) State.Success = 1
+BadState1.Fail; // (enum member) State.Fail = 2
+
+const badCheckState = (state: BadState1) => {
+  //
+};
+
+badCheckState(100); // not error
+
+
+//Good ✅
+type GoodState = "InProgress" | "Success" | "Fail";
+enum GoodState2 {
+  InProgress = "InProgress",
+  Success = "Success",
+  Fail = "Fail",
+}
+
+const goodCheckState = (state: GoodState2) => { };
+
+goodCheckState("afjalfkj"); // error
