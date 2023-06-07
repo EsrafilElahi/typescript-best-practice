f1a6d72a04a7bbc877a77e8f8d6e786bd243d280
esrafilelahi <essinho1121@gmail.com>
2023-06-07 11:39:19 +0330

    editing....

--- short_videos.ts
+++ short_videos.ts
@@ -23,6 +23,39 @@ const map3 = new Map<string, string>()
 
 // ========================================================================================
 
+interface IPerson {
+  address?: {
+    city?: string
+  }
+}
+
+const person: IPerson = {}
+
+const city1 = person.address.city // error
+const city2 = person.address?.city // ? optional chaining
+const city3 = person.address!.city // ! undefined chaining
+
+// ========================================================================================
+
+interface IPerson {
+  name: string,
+  age: number
+}
+
+// mapped type custom partial-type
+type IPartialPerson<T> = {
+  [P in keyof T]?: T[P] | undefined
+}
+
+const person2: Partial<IPerson> = {}
+const person3: IPartialPerson<IPerson> = {}
+
+// ========================================================================================
+
+
+
+// ========================================================================================
+
 
 
 // ========================================================================================
