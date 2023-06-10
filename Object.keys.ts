170bd029aee0a5a80c84ce2192e8c1dd126cf80c
esrafilelahi <essinho1121@gmail.com>
2023-06-10 17:18:39 +0330

    add object.keys

---
+++ Object.keys.ts
@@ -0,0 +1,13 @@
+const myObj = {
+  a: 1,
+  b: 2,
+  c: 3
+}
+
+const objectKeys = <Obj>(obj: Obj): (keyof Obj)[] => {
+  return Object.keys(obj) as (keyof Obj)[];
+}
+
+objectKeys(myObj).forEach(item => {
+  console.log(myObj[item]);
+})
