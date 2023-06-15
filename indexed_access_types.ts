d07ea4698365463d886f2502a61733618fcdf5f3
esrafilelahi <essinho1121@gmail.com>
2023-06-16 01:30:13 +0330

    editting...

--- indexed_access_types.ts
+++ indexed_access_types.ts
@@ -5,17 +5,17 @@ interface ColorVariants {
   tertiary: "green"
 }
 
-type PrimaryColor = ColorVariants["primary"]
-type NonPrimaryColor = ColorVariants["secondary" | "tertiary"]
-type EveryColorVariants = ColorVariants[keyof ColorVariants]
+type PrimaryColor = ColorVariants["primary"] // "blue"
+type NonPrimaryColor = ColorVariants["secondary" | "tertiary"] // "red" | "green"
+type EveryColorVariants_Value = ColorVariants[keyof ColorVariants] // "blue" | "red" | "green"
+type EveryColorVariants_Key = keyof ColorVariants // "primary" "secondary" | "tertiary"
 
 
 // letters
 type Letters = ["a", "b", "c"]
- 
-type AOrB = Letters["0" | "1"]
-type AllLettersUnion = Letters[number]
-// "a" | "b" | "c"
+
+type AOrB = Letters["0" | "1"] // "a" | "b"
+type AllLettersUnion = Letters[number] // "a" | "b" | "c"
 
 
 // userRole
