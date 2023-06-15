4842cd536ece540adb1da31fa153d1045170db00
esrafilelahi <essinho1121@gmail.com>
2023-06-16 01:14:02 +0330

    add mapped type file

---
+++ mapped_type.ts
@@ -0,0 +1,13 @@
+interface ApiData {
+  "map:lat": string,
+  "map:long": string,
+  "exist": boolean
+}
+
+type RemoveMapFromObj<Obj> = {
+  [Key in keyof Obj as ExtractLatLong<Key>]: Obj[Key]
+}
+
+type DesiredShape = RemoveMapFromObj<ApiData>;
+
+type ExtractLatLong<Key> = Key extends `map:${infer U}` ? U : Key;
