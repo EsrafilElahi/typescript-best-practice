9c9bf7e6593bf8dd7b04cd9aed0e30acf77be035
esrafilelahi <essinho1121@gmail.com>
2023-06-16 00:49:19 +0330

    add file

---
+++ generix_advanced.ts
@@ -0,0 +1,8 @@
+type RemoveKeyResultType<Key extends string, Obj> = Omit<Obj, Key>
+export const makeKeyRemover = <Key extends string>(keys: Key[]) => <Obj>(obj: Obj): RemoveKeyResultType<Key, Obj> => { return {} as any }
+
+const keyRemover = makeKeyRemover(["a", "b"])
+
+const newObject = keyRemover({ a: 1, b: 2, c: 3 })
+
+newObject.c // just this show
