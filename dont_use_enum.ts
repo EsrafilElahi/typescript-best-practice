4bc8f7498e791394629d5bff6384d85cf4e668fe
esrafilelahi <essinho1121@gmail.com>
2023-06-16 02:00:38 +0330

    add file don't use enums anymore

---
+++ dont_use_enum.ts
@@ -0,0 +1,16 @@
+const LOG_LEVEL = {
+  DEBUG: "DEBUG",
+  WARNING: "WARNING",
+  ERROR: "ERROR",
+} as const
+
+type ObjectValues<Obj> = Obj[keyof Obj];
+
+type LogLevel = ObjectValues<typeof LOG_LEVEL>
+
+function log(msg: string, level: LogLevel) {
+  // code
+}
+
+log("hi", "WARNING") // no err -----> //? doesn't required to export this LOG_LEVEL anywhere, we can just pass the values in params
+log("hi", LOG_LEVEL.WARNING) // we can use directly, no err too
