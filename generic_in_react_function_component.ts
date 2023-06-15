b248e16105e2ac994adffde92c1948ac35351204
esrafilelahi <essinho1121@gmail.com>
2023-06-15 23:35:48 +0330

    add file

---
+++ generic_in_react_function_component.ts
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+interface TableProps<TItem> {
+  items: TItem,
+  renderItem: (item: TItem) => React.ReactNode
+}
+
+// get TItem type from where use this component
+export function Table<TItem>(props: TableProps<TItem>) {
+  return null
+}
+
+
+const App = () => {
+  return (
+    <div>
+
+      <Table<{ id: number, name: string }> // define TItem here
+        items= { [{ id: 1, name: 'joe' }] }
+        renderItem={(item) => <div>{item}</div>}
+      />
+
+    < /div>
+  )
+}
