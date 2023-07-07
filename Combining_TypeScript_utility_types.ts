224d608356be9ab3bd664bb7802cf2b1cf4dcf9a
Esrafil <essinho1121@gmail.com>
2023-07-07 17:16:21 +0330

    Update Combining_TypeScript_utility_types.ts

--- Combining_TypeScript_utility_types.ts
+++ Combining_TypeScript_utility_types.ts
@@ -5,7 +5,16 @@ interface User {
   age?: number;
 }
 
+// not reusable
 type LoggedUser1 = Partial<User> & Required<Pick<User, 'id'>>;
 type LoggedUser2 = Required<Pick<User, 'id'>> & Omit<User, 'id'>;
 
 
+// reusable
+type RequireFields<T, K extends keyof T> = Required<Pick<T, K>> & Omit<T, K>;
+const u2: RequireFields<User, 'id' | 'age'> = {
+  id: 2,
+  age: 32,
+  firstname: 'Chris',
+  lastname: 'Bongers',
+};
