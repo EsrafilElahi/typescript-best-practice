571ba5cf5155f9120e313157749c9791b1c7ad65
Esrafil <essinho1121@gmail.com>
2023-07-07 16:06:14 +0330

    Create merge_types.ts

---
+++ merge_types.ts
@@ -0,0 +1,26 @@
+import React from "react";
+
+type ShapeType = "cube" | "square" | "rectangle" | "triangle";
+type TwoDShapeType<T> = Exclude<ShapeType, T>;
+
+type ThemeType = "dark" | "light";
+type ColorType = "red" | "blue" | "yellow";
+
+type ItemProps = {
+  // color: `${ThemeType}-${ColorType}`;
+  color: Exclude<`${ThemeType}-${ColorType}`, "dark-yellow">;
+};
+
+const Shape = (props:ItemProps) => {
+  const chosenShape: ShapeType = "cube";
+  const twoDShape: TwoDShapeType<chosenShape> = "square";
+
+  return <div>Shape</div>;
+};
+
+export default Shape;
+
+
+// use Shape component:
+<Shape color="dark-blue"/>
+<Shape color="light-yellow"/>
