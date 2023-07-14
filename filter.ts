7f063d7533bac3c9a7497e307fe934ae2a442295
Esrafil <essinho1121@gmail.com>
2023-07-14 23:35:54 +0330

    Create filter.ts

---
+++ filter.ts
@@ -0,0 +1,29 @@
+type Person = PersonA | PersonB;
+
+interface PersonA {
+  type: 'A';
+  name: string;
+  age: number;
+}
+
+interface PersonB {
+  type: 'B';
+  name: string;
+  city: string;
+  isAdmin: boolean;
+}
+
+const people: Person[] = [
+  { type: 'A', name: 'Alice', age: 25 },
+  { type: 'B', name: 'Bob', city: 'London', isAdmin: true },
+  { type: 'A', name: 'Charlie', age: 30 },
+];
+
+// Example 1: Filter PersonA objects
+const personAs: PersonA[] = people.filter((person): person is PersonA => person.type === 'A');
+
+// Example 2: Filter PersonB objects
+const personBs: PersonB[] = people.filter((person): person is PersonB => person.type === 'B');
+
+console.log(personAs);
+console.log(personBs);
