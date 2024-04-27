e461b844e9a01a97ae21291d58bebb4a4d7cdbdf
Esrafil <essinho1121@gmail.com>
2024-04-27 11:23:27 +0330

    Update useRef in react.ts

--- useRef in react.ts
+++ useRef in react.ts
@@ -5,3 +5,20 @@ const Component = () => {
 
   return <audio ref={audioRef}> Audio Tag </audio>;
 };
+
+
+function useRef<T>(initialValue: T): MutableRefObject<T>;
+function useRef<T>(initialValue: T|null): RefObject<T>;
+function useRef<T = undefined>(): MutableRefObject<T | undefined>;
+
+interface RefObject<T> {
+  readonly current: T | null;
+}
+
+function A() {
+  const ref1 = useRef<HTMLParagraphElement>(null)    
+  // const ref1: React.RefObject<HTMLParagraphElement>
+
+  const ref2 = useRef<ElementRef<'p'>>(null)
+  // const ref2: React.RefObject<HTMLParagraphElement>
+}
