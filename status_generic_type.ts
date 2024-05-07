e56c44c578d13b1bb924cce6e7a00ea3e71b0c9b
Esrafil <essinho1121@gmail.com>
2024-05-07 09:40:37 +0330

    Update status_generic_type.ts

--- status_generic_type.ts
+++ status_generic_type.ts
@@ -4,4 +4,4 @@ type ExcludeStatus<T extends string> = T | Omit<string, T>
 type statusType = ExcludeStatus<"pending" | "done" | "reject">
 
 const status11: statusType = 'reject' // ok
-const status12:statusType = 'messi' // ok
+const status12:statusType = 'messi' // ok 
